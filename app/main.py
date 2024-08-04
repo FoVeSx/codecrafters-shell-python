ffa60bb13af2693275a2c20c87ff73acce6ea8ec
FoVeSx <taariq.chasmawala@gmail.com>
2024-08-03 23:32:29 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,17 +1,25 @@
 # codecrafters test
 # codecrafters test # Visit https://codecrafters.io/cli to install
+# A REPL is an interactive loop that reads user input, evaluates it, prints the result, 
+# and then waits for the next input.
+
 
 import sys
 
 def main():
-    valid_commands = ["echo", "cd", "ls"]
+    valid_commands = ["echo", "cd", "ls", "exit"]
 
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while(True):
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    command = input()
-    if command not in valid_commands:
-        sys.stdout.write(f"{command}: command not found\n")
+        command = input()
+
+        if command not in valid_commands:
+            sys.stdout.write(f"{command}: command not found\n")
+
+        if command == "exit":
+            break
 
 if __name__ == "__main__":
     main()
