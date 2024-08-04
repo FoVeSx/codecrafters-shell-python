601e1f5e06f3608f3dd12b7e5f7942923fccda39
FoVeSx <taariq.chasmawala@gmail.com>
2024-08-04 00:07:16 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,24 +2,51 @@
 # codecrafters test # Visit https://codecrafters.io/cli to install
 # A REPL is an interactive loop that reads user input, evaluates it, prints the result, 
 # and then waits for the next input.
-
-
 import sys
 
-def main():
-    valid_commands = ["echo", "cd", "ls", "exit"]
+valid_commands = ["echo", "cd", "ls", "exit"]
 
+def command_handler(command: str, options: list):
+    """
+    Command Handler that will redirect program to command executor. 
+    Checks that command is valid and exists.
+
+    :param command: string of the command (program) to be ran
+    :param options: list of arguments passed into command
+    :return
+    """
+    if command not in valid_commands:
+        sys.stdout.write(f"{command}: command not found\n")
+        return
+
+    if command == "exit":
+        if len(options) == 0:
+            sys.exit(0)
+        else:
+            sys.exit(int(options[0]))
+
+    return
+
+def command_parser(command_line: str):
+    """
+    Parse the entire command for the command and its options.
+    
+    :param command_line: string of the entire command line inputted
+    :return tuple: command string and options list
+    """
+    command_list = command_line.split(" ")
+    command = command_list[0]
+    options = command_list[1:]
+    return command, options
+
+def main():
     while(True):
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
-        command = input()
-
-        if command not in valid_commands:
-            sys.stdout.write(f"{command}: command not found\n")
-
-        if command == "exit":
-            break
+        command_input = input()
+        command, options = command_parser(command_input)
+        command_handler(command, options)
 
 if __name__ == "__main__":
     main()
