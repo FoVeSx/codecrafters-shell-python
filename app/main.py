73a14f208e510c7453c23cdc727863bb4d86d3a0
FoVeSx <taariq.chasmawala@gmail.com>
2024-08-03 23:28:24 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def main():
 
     command = input()
     if command not in valid_commands:
-        sys.stdout.write(f"{command}: command not found")
+        sys.stdout.write(f"{command}: command not found\n")
 
 if __name__ == "__main__":
     main()
